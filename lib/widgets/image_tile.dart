ed81f063acdb9e22f4aa61b89b25a17f1f9f5781
Anirban Dey <anirbandey@soufrabi.com>
2024-10-25 17:49:38 +0530

    Save wallpaper to Downloads directory

--- lib/widgets/image_tile.dart
+++ lib/widgets/image_tile.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class ImageTile extends StatelessWidget {
   final String imageSource;
@@ -14,6 +19,10 @@ class ImageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    showSnackBarMessage(String message) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(message)));
+    }
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
@@ -38,14 +47,36 @@ class ImageTile extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "Art Style ${index+1}",
+                    "Art Style ${index + 1}",
                     style: const TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
                   InkWell(
-                    onTap: (){
+                    onTap: () async {
+                      try {
+                        final Directory? downloadsDir =
+                        await getDownloadsDirectory();
+                        if (downloadsDir == null) {
+                          throw Exception("failed to get download directory");
+                        }
+                        const String appName = "mkbhd_panels";
+                        final String appDownloadsDirPath =
+                            "${downloadsDir.path}/$appName";
+                        final Directory appDownloadsDir =
+                        await Directory(appDownloadsDirPath).create();
+                        final String filename =
+                            "${const Uuid().v5(Uuid.NAMESPACE_URL,
+                            imageSource)}.jpg";
+                        final path = "${appDownloadsDir.path}/$filename";
+                        final file = File(path);
+                        final res = await get(Uri.parse(imageSource));
+                        file.writeAsBytes(res.bodyBytes);
+                        showSnackBarMessage("File saved as ${file.path}");
+                      } catch (e) {
+                        showSnackBarMessage("Error : ${e.toString()}");
+                      }
                     },
                     child: const Icon(
                       Icons.favorite_outline,
