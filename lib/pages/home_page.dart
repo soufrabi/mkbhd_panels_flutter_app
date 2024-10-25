f146dc157858a7bb5ca5d649c57a93c6316c87b7
Anirban Dey <anirbandey@soufrabi.com>
2024-10-25 12:07:25 +0530

    Add tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,18 +8,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MasonryGridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 12,
-        crossAxisSpacing: 12,
-        padding: const EdgeInsets.all(12),
-        itemBuilder: (context, index) {
-          return ImageTile(
-            imageSource: "https://picsum.photos/seed/${index+1}/500/1000",
-            index: index,
-            extent: ( (index & 1) == 0 ) ? 300 : 150,
-          );
-        },
+      body: DefaultTabController(
+        length: 3,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                floating: true,
+                snap: true,
+                title: Center(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                bottom: const TabBar(tabs: [
+                  Tab(
+                    text: "Suggested",
+                  ),
+                  Tab(
+                    text: "Liked",
+                  ),
+                  Tab(
+                    text: "Library",
+                  ),
+                ]),
+              )
+            ];
+          },
+          body: TabBarView(
+            children: [
+              // Tab 1
+              MasonryGridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                padding: const EdgeInsets.all(12),
+                itemBuilder: (context, index) {
+                  return ImageTile(
+                    imageSource:
+                        "https://picsum.photos/seed/${index + 1}/500/1000",
+                    index: index,
+                    extent: ((index & 1) == 0) ? 300 : 150,
+                  );
+                },
+              ),
+              // Tab 2
+              const SizedBox(),
+              // Tab 3
+              const SizedBox(),
+            ],
+          ),
+        ),
       ),
     );
   }
